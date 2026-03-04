b1afef7707c822e1627094c8a9a96eb8711cf2b9
EricChen34312 <85235002+EricChen34312@users.noreply.github.com>
2026-03-04 15:05:19 +0000

    Solve Q1-Q2

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,78 @@
+import java.io.*;
+import java.io.IOException;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	
+	public static String readFile(String filename){
+		//Initiate bufferedreader or something (and the output string)
+		BufferedReader reader = null;
+		String out = "";
 
+		try {
+
+			//make the thing be able to read the file
+			reader = new BufferedReader(new FileReader(filename));
+			String line;
+
+			//if there is a line then output that line plus a new line character
+			while((line = reader.readLine()) != null){
+				out += line + '\n';
+			}
+		}
+		catch (IOException e){
+			System.out.println("uh oh");
+		}
+		finally {
+			try {
+				//close the darn thing
+				if(reader != null){
+					reader.close();
+				}
+			}
+			catch(IOException e){
+				//how does this even happen
+				System.out.println("super uh oh");
+			}
+		}
+
+		return out;
+	}
+
+	public static String backwardsReadFile(String filename){
+		//initiate the thing but use filereader instead for variety fr
+		FileReader reader = null;
+		String out = "";
+
+		try{
+
+			//try to read the thing and store char 
+			reader = new FileReader(filename);
+			int c;
+
+			//append the output string to the char if not null
+			while((c = reader.read()) != -1){
+				out = ((char) c) + out;
+			}
+		} 
+		catch (IOException e){
+			System.out.println("uh oh");
+		}
+		finally {
+			try{
+				//close reader if ever opened
+				if(reader != null){
+					reader.close();
+				}
+			}
+			catch(IOException e) {
+				System.out.println("super uh oh");
+			}
+		}
+
+		return out;
+	}
 }
